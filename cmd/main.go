53b59b259e510977671d7a0ce59dd04ad956aa2a
bashery <ahmedalbachiry@gmail.com>
2023-12-01 15:01:45 +0300

    using number as key instead string

--- cmd/main.go
+++ cmd/main.go
@@ -6,34 +6,31 @@ import (
 	"time"
 )
 
-var max = 100000
+var max = 10000
 
 // main
 func main() {
 
 	// first db
 	db := kvlite.Open("db1/")
-
-	l := 0
+	defer db.Close()
 
 	s := time.Now()
 	for i := 0; i < max; i++ {
-		key := "users:" + fmt.Sprint(i)
-		db.Set(key, "hello from db1 id"+key)
+		db.Set(i, "hello world:"+fmt.Sprint(i))
 	}
 	fmt.Println(time.Since(s))
-
-	time.Sleep(time.Second)
 	s = time.Now()
 
-	l = 0
-	for i := 0; i < max; i++ {
-		l += len(db.Get("users:" + fmt.Sprint(i)))
-	}
+	// set data
 
+	l := 0
+	for i := 0; i < max; i++ {
+		l += len(db.Get(i))
+	}
 	fmt.Println(time.Since(s))
 
-	fmt.Println("\n\ndata & size: ", db.Get("users:2999"), l)
-	time.Sleep(time.Second)
+	data := db.Get(123)
+	fmt.Println("len & data:", l, data)
 
 }
